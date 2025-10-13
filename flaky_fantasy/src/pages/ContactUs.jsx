6a69fc077fd01f20c819f4d29a0987c5395331d3
chewi_clinton <chewiclinton.shu@ictuniversity.edu.cm>
2025-10-13 15:59:44 +0100

    .

--- flaky_fantasy/src/pages/ContactUs.jsx
+++ flaky_fantasy/src/pages/ContactUs.jsx
@@ -190,10 +190,10 @@ const ContactUs = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d…!2d11.49253!3d3.85734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMy44NTczNCwgMTEuNDkyNTM!5e0!3m2!1sen!2sus!4v1234567890"
               width="100%"
               height="350"
-              style="border:0;"
-              allowfullscreen=""
+              style={{ border: 0 }}
+              allowFullScreen=""
               loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
+              referrerPolicy="no-referrer-when-downgrade"
               title="Carrefour Obili Location Map"
             ></iframe>
           </div>
