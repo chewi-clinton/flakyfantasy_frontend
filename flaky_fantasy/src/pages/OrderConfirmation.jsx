2298ed402bd382e50ed017edc69237ee5462982d
chewi-clinton <suhchenwi48@gmail.com>
2025-10-19 14:58:04 +0100

    .

--- flaky_fantasy/src/pages/OrderConfirmation.jsx
+++ flaky_fantasy/src/pages/OrderConfirmation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/OrderConfirmation.css";
+import "../styles/Orderconfirmation.css";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
